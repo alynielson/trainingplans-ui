54e1a8b9f423fe20be42fda51d7131ddabef1c53
alynielson <nielsonaly@gmail.com>
2020-07-16 22:20:34 -0500

    make table row height consistent

--- src/components/calendarView/dayInMonth/dayInMonth.js
+++ src/components/calendarView/dayInMonth/dayInMonth.js
@@ -5,8 +5,24 @@ const DayInMonth = (props) => {
     return (
       <td className="cell">
           <div className="corner">{props.day === 0 ? null : props.day}</div>
+
+            <Test day={props.day} />
       </td>  
     );
+}
+
+const Test = (props) => {
+  if (props.day !== 6) {
+    return null;
+  }
+  return (
+    <div className="cell-contents">
+      <ul>
+        <li>test</li>
+        <li>t2</li>
+      </ul>
+    </div>
+  );
 }
 
 export default DayInMonth;
